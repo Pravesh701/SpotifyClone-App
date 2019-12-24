6df8ea297c74be1b00d4b9cad41e0e30e19b0db5
Admin <admin@Pro-User.local>
2019-12-24 16:02:44 +0530

    base

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import React, {Component} from 'react';
-//import MAinContainer from './src/navigators/rootNavigator';
-import Splash from './src/constants/splashScreen/splash';
 import SplashSpotify from './src/components/Spotify/splash_Spotify';
-
 import MAinContainer from './src/navigators/mainNavigators';
-
 export default class App extends Component {
   constructor() {
     super();
